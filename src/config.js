537e406c6442f04f36dd0f01b3e8e585d87c47ed
Shunsuke Ise <ise@ebiiim.com>
2021-02-12 02:39:37 +0900

    misc: use .env

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export const cfg = {
+  "api": {
+    "ledgers": {
+      "server": process.env.REACT_APP_API_LEDGERS_SERVER,
+      "baseUrl": process.env.REACT_APP_API_LEDGERS_BASE,
+    },
+    "apikeys": {
+      "server": process.env.REACT_APP_API_APIKEYS_SERVER,
+      "baseUrl": process.env.REACT_APP_API_APIKEYS_BASE,
+    },
+  },
+};
