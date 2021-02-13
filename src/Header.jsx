dad7780a8b053fb316d31e70d83c258d7c2397b8
Shunsuke Ise <ise@ebiiim.com>
2021-02-14 06:38:04 +0900

    misc: link title area to top page

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,12 +2,14 @@ export default function Header() {
   return (
     <header className="hero is-info">
       <div className="hero-body">
-        <h1 className="title">
-          BBc-1 Ledger Explorer
-        </h1>
-        <h2 className="subtitle">
-          BBc-1 Ledger Subsystem with Bitcoin Anchor
-        </h2>
+        <a href="/">
+          <h1 className="title">
+            BBc-1 Ledger Explorer
+          </h1>
+          <h2 className="subtitle">
+            BBc-1 Ledger Subsystem with Bitcoin Anchor
+          </h2>
+        </a>
       </div>
     </header>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import "bulma/css/bulma.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Header from "./Header";
 import Footer from "./Footer";
@@ -12,9 +12,11 @@ const app = document.querySelector("#app")
 ReactDOM.render(
   <Router>
     <Header />
-    <Route exact path="/" component={SectionTopPage}></Route>
-    <Route exact path="/ledgers" component={SectionGetDomAndTx}></Route>
-    <Route path="/ledgers/domains/:dom/transactions/:tx" component={SectionAnchorRecord}></Route>
-    <Route exact path="/apikeys" component={SectionGetAPIKey}></Route>
+    <Switch>
+      <Route exact path="/" component={SectionTopPage}></Route>
+      <Route exact path="/ledgers" component={SectionGetDomAndTx}></Route>
+      <Route path="/ledgers/domains/:dom/transactions/:tx" component={SectionAnchorRecord}></Route>
+      <Route exact path="/apikeys" component={SectionGetAPIKey}></Route>
+    </Switch>
     <Footer />
   </Router>, app);
